c2ecd589463601b21a7b67c005cba844dc6123df
Thiago Assis <thiagosil@gmail.com>
2020-06-04 17:14:02 -0300

    More refactorings

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -14,7 +15,9 @@ usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+    const usersRepository = new UsersRepository();
+
+    const createUser = new CreateUserService(usersRepository);
     const user = await createUser.execute({ name, email, password });
 
     delete user.password;
@@ -30,7 +33,9 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const userAvatarService = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+
+    const userAvatarService = new UpdateUserAvatarService(usersRepository);
     const user = await userAvatarService.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
