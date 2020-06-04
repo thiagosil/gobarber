ded4c380a3321111b6b1874d920cd3c284fa2933
Thiago Assis <thiagosil@gmail.com>
2020-06-04 17:41:14 -0300

    Injecting repositories

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-
 import multer from 'multer';
+import { container } from 'tsyringe';
+
 import uploadConfig from '@config/upload';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -15,9 +15,7 @@ usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
-    const usersRepository = new UsersRepository();
-
-    const createUser = new CreateUserService(usersRepository);
+    const createUser = container.resolve(CreateUserService);
     const user = await createUser.execute({ name, email, password });
 
     delete user.password;
@@ -33,9 +31,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-
-    const userAvatarService = new UpdateUserAvatarService(usersRepository);
+    const userAvatarService = container.resolve(UpdateUserAvatarService);
     const user = await userAvatarService.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
