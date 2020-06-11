4d5b80b0450c9346f38c37276140ad252412c94f
Thiago Assis <thiagosil@gmail.com>
2020-06-11 19:01:42 -0300

    removing past hours

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,48 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let listAvailability: ListProviderDayAvailabilityService;
+let fakeAppointmentRepository: FakeAppointmentRepository;
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository();
+    listAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentRepository,
+    );
+  });
+
+  it('should be able list the day availability for provider', async () => {
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+
+    const availability = await listAvailability.execute({
+      provider_id: 'provider',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+      ]),
+    );
+  });
+});
---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,75 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let listAvailability: ListProviderMonthAvailabilityService;
+let fakeAppointmentRepository: FakeAppointmentRepository;
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository();
+    listAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentRepository,
+    );
+  });
+
+  it('should be able list the month availability for provider', async () => {
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 17, 0, 0),
+    });
+    await fakeAppointmentRepository.create({
+      provider_id: 'provider',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    const availability = await listAvailability.execute({
+      provider_id: 'provider',
+      year: 2020,
+      month: 5,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 2, available: true },
+      ]),
+    );
+  });
+});
