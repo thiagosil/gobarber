e7b7f13f9f6a4f2ddd12431bacdfefd4d85deb0a
Thiago Assis <thiagosil@gmail.com>
2020-06-14 16:05:34 -0300

    Creating appointmen notifications

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -43,6 +43,10 @@ class AppointmentsRepository implements IAppointmentsRepository {
     const appointments = await this.ormRepository.find({
       where: {
         provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+        ),
       },
     });
 
